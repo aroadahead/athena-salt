b188ac918e94b80e07b298bffd4dcc440ed5f24b
yonatonreid <yonatonreid@gmail.com>
2022-03-22 00:48:41 -0400

    doctrine conn

---
+++ src/Module.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AthenaSalt;
+
+use AthenaCore\Mvc\Application\Modules\AbstractModule;
+
+class Module extends AbstractModule
+{
+    protected string $dir = __DIR__;
+}
